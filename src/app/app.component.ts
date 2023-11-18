ebec303d8e35edbd65c5e58c441f307419eff92c
lealeleu <lealeleu.software@gmail.com>
2023-11-18 15:32:22 +0100

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: ` <h1>Welcome to {{ pokemonList [0]}}!</h1>`
+})
+export class AppComponent implements OnInit {
+  pokemonList = ['Bulbizarre', 'Salamèche', 'Carapuce'];
+
+  ngOnInit() {
+    console.table(this.pokemonList);
+  }
+
+  selectPokemon(pokemonName: string) {
+    console.log(`Vous avez cliqué sur le pokémon ${pokemonName}`);
+  }
+}
